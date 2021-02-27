92e0f659e6672411e55dd2d9311d4f19ea1d2b31
Jesse Schulman <jesse@caleos.io>
2021-02-26 16:48:50 -0800

    Return proper hash

--- src/telos.ts
+++ src/telos.ts
@@ -124,7 +124,7 @@ export class TelosApi {
 
     let trx = new Transaction(`0x${tx}`, { common: this.chainConfig })
     response.eth = {
-      transactionHash: trx.hash
+      transactionHash: trx.hash().toString('hex')
     }
 
     /*
