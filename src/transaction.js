a0c119193df542834a1fe6bb4206ba3f4e90af0a
Jesse Schulman <jesse@caleos.io>
2021-06-30 09:48:43 -0700

    More dependency cleanup

--- src/transaction.js
+++ src/transaction.js
@@ -61,8 +61,8 @@ transactions: Array - Array of transaction objects, or 32 Bytes transaction hash
 uncles: Array - Array of uncle hashes.
 */
 
-const { Transaction } = require('ethereumjs-tx')
-const Common = require('ethereumjs-common')
+const { Transaction } = require('@ethereumjs/tx')
+const Common = require('@ethereumjs/common')
 const { keccak256 } = require('ethereumjs-util')
 const { TextEncoder } = require('text-encoding')
 const LOGS_BLOOM_EMPTY =
