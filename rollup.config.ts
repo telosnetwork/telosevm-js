4fa80b889b08b7650ecdf0fb037d5ba9699562fb
Jesse Schulman <jesse@caleos.io>
2021-06-30 10:48:57 -0700

    Remove rollups for now, maybe it fixes stuff

--- rollup.config.ts
+++ rollup.config.ts
@@ -17,7 +17,7 @@ export default {
     { file: pkg.module, format: 'es', sourcemap: true },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: ['ethereumjs-util', 'ethereumjs-abi'],
+  external: [],
   watch: {
     include: 'src/**',
   },
